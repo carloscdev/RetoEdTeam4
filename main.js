ba8d201424e478aa02dff921abda4f279f0265fb
Carlos Córdova <ccordova.zosma@gmail.com>
2021-03-05 22:39:07 -0500

    Reto EDTeam N° 4

---
+++ main.js
@@ -0,0 +1,14 @@
+const questionItem = document.getElementsByClassName(
+  "questions__container--item"
+);
+document.querySelectorAll(".item").forEach((el) => {
+  el.addEventListener("click", function addRemoveClass() {
+    const indexItem = this.parentNode.className;
+    for (let i = 0; i < questionItem.length; i++) {
+      questionItem[i].className = "questions__container--item";
+    }
+    if (indexItem == "questions__container--item") {
+      this.parentNode.className = "questions__container--item active";
+    }
+  });
+});
